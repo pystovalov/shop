4a2353a760fc34bb69530622abcf277292ff4eff
pystovalov <a.a.pystovalov@yandex.ru>
2025-03-06 11:57:47 +0300

    base settings

--- base/products/views.py
+++ base/products/views.py
@@ -1,3 +1,10 @@
+from django.http.response import re
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "products/index.html")
+
+
+def products(request):
+    return render(request, "products/products.html")

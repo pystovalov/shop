d7819bdcd2d4d8824dddae0669650bda01584c7e
pystovalov <a.a.pystovalov@yandex.ru>
2025-03-16 16:56:04 +0300

    create authontication

--- base/users/views.py
+++ base/users/views.py
@@ -1,18 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+    else:
+        form = UserLoginForm()
     context = {
-        "form": UserLoginForm(),
+        "form": form,
     }
     return render(request, "users/login.html", context)
 
 
 def register(request):
-    return render(request, "users/register.html")
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "users/register.html", context)
 
 
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        form = UserProfileForm(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:profile"))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {"form": form}
+    return render(request, "users/profile.html", context)
